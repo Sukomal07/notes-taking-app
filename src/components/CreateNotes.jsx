b14ae04277bc7f10deab0f4d32dacf0401d5a41d
Sukomal07 <hypersukomal@gmail.com>
2023-10-26 23:57:29 +0530

    add create note and responsive design

--- src/components/CreateNotes.jsx
+++ src/components/CreateNotes.jsx
@@ -1,33 +1,92 @@
-import AllColors from '../helpers/colors.json'
-import "../styles/CreateNotes.css"
-function CreateNotes() {
+import { useState } from 'react';
+import AllColors from '../helpers/colors.json';
+import '../styles/CreateNotes.css';
+
+function CreateNotes({ setOpen }) {
+    const [groupName, setGroupName] = useState('');
+    const [selectedColor, setSelectedColor] = useState('');
+    const [groupNameError, setGroupNameError] = useState(false);
+    const [colorError, setColorError] = useState(false);
+
+    const handleGroupNameChange = (e) => {
+        setGroupName(e.target.value);
+        setGroupNameError(false);
+    };
+
+    const handleColorSelection = (color) => {
+        setSelectedColor(color);
+        setColorError(false);
+    };
+
+    const handleCreateGroup = (e) => {
+        e.preventDefault();
+
+        if (!groupName) {
+            setGroupNameError(true);
+        }
+
+        if (!selectedColor) {
+            setColorError(true);
+        }
+
+        if (groupName && selectedColor) {
+            const newGroup = {
+                groupname: groupName,
+                color: selectedColor,
+                groupnotes: [],
+            };
+
+            const existingGroups = JSON.parse(localStorage.getItem('groups')) || [];
+            const updatedGroups = [...existingGroups, newGroup];
+            localStorage.setItem('groups', JSON.stringify(updatedGroups));
+
+            setOpen(false);
+        }
+    };
+
     return (
-        <div className="modal">
-            <div className="modal-content">
-                <span className="close">
+        <div className="model">
+            <div className="model-content">
+                <span className="close" onClick={() => setOpen(false)}>
                     &times;
                 </span>
-                <form>
+                <form onSubmit={handleCreateGroup}>
                     <h1>Create New Notes group</h1>
                     <div className="inputContainer">
                         <div className="groupName">
                             <label htmlFor="groupName">Group Name</label>
-                            <input type="text" placeholder="Enter your group name..." />
+                            <input
+                                type="text"
+                                id="groupName"
+                                name="groupName"
+                                placeholder="Enter your group name..."
+                                value={groupName}
+                                onChange={handleGroupNameChange}
+                                style={{ border: groupNameError ? '1px solid red' : '1px solid #CCCCCC' }}
+                            />
                         </div>
                         <div className="groupColor">
-                            <label htmlFor="groupColor">Choose Color</label>
+                            <label>Choose Color</label>
                             <div className="colors">
                                 {AllColors.map((color) => (
-                                    <span key={color.id} style={{ backgroundColor: color.color }}></span>
+                                    <span
+                                        key={color.id}
+                                        style={{
+                                            backgroundColor: color.color,
+                                            border: colorError ? '2px solid red' : ''
+                                        }}
+                                        className={selectedColor === color.color ? 'selected' : ''}
+                                        onClick={() => handleColorSelection(color.color)}
+                                    ></span>
                                 ))}
                             </div>
                         </div>
                     </div>
-                    <button>Create</button>
+                    <button type="submit">Create</button>
                 </form>
             </div>
         </div>
-    )
+    );
 }
 
-export default CreateNotes
+export default CreateNotes;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,25 +1,42 @@
+import { useEffect, useState } from "react"
 import "../styles/List.css"
+import CreateNotes from "./CreateNotes";
 
 
 function List() {
-    // const logo = notesName?.split(" ").map(word => word[0]).join('');
+    const [open, setOpen] = useState(false);
+    const [groups, setGroups] = useState([]);
+
+    const getGroupsFromLocalStorage = () => {
+        const storedGroups = JSON.parse(localStorage.getItem("groups")) || [];
+        setGroups(storedGroups);
+    };
+
+    useEffect(() => {
+        getGroupsFromLocalStorage();
+    }, [open]);
     return (
-        <div className="listContainer">
-            <h3>Pocket Notes</h3>
-            <div className="notesGroup">
-                <div className="createNote">
-                    <button>+ Create Notes Group</button>
+        <>
+            <div className="listContainer">
+                <h3>Pocket Notes</h3>
+                <div className="notesGroup">
+                    <div className="createNote">
+                        <button onClick={() => setOpen(true)}> <span>+</span> Create Notes Group</button>
+                    </div>
+                    <ul>
+                        {groups.map((group, index) => (
+                            <li key={index}>
+                                <div className="logo" style={{ backgroundColor: group?.color }}>
+                                    <span>{group?.groupname?.split(" ").map(word => word[0]).join('')}</span>
+                                </div>
+                                <p>{group?.groupname}</p>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-                <ul>
-                    <li>
-                        <div className="logo" >
-                            <span>{ }</span>
-                        </div>
-                        <p>{ }</p>
-                    </li>
-                </ul>
             </div>
-        </div>
+            {open && <CreateNotes setOpen={setOpen} />}
+        </>
     )
 }
 
